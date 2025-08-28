8a8a6dac1de7a23d5f26b7a2db6925e9a20391c2
Nurlan Purkhanov <ppurkhanov@gmail.com>
2025-08-28 17:27:41 +0500

    deleted getUpdates from bot.go

--- bot/bot.go
+++ bot/bot.go
@@ -4,15 +4,12 @@ import (
 	"errors"
 	"fmt"
 	"net/http"
-	"net/url"
 
 	"github.com/purkhanov/gogram/api"
-	"github.com/purkhanov/gogram/types"
 )
 
 const (
 	baseUrl         = "https://api.telegram.org"
-	getUpdatesUrl   = "/getUpdates"
 	contentTypeJSON = "application/json"
 )
 
@@ -30,25 +27,4 @@ func NewBot(token string) *Bot {
 		urlWithToken: fmt.Sprintf("%s/bot%s", baseUrl, token),
 		api:          api.NewClient(&http.Client{}),
 	}
-}
-
-func (b *Bot) GetUpdates(params url.Values) ([]types.Update, error) {
-	req, err := http.NewRequest(http.MethodGet, b.urlWithToken+getUpdatesUrl, nil)
-	if err != nil {
-		return nil, fmt.Errorf("failed to create request: %v", err)
-	}
-	req.URL.RawQuery = params.Encode()
-	req.Header.Set("Content-Type", contentTypeJSON)
-
-	resp, err := b.api.DoRequest(req)
-	if err != nil {
-		return nil, err
-	}
-
-	result, ok := resp.Result.([]types.Update)
-	if !ok {
-		return nil, errInvalidType
-	}
-
-	return result, nil
 }
