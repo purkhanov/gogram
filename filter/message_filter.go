1104de01327cdc27e7c8cfed08d1eb7d738ce1c3
Nurlan Purkhanov <ppurkhanov@gmail.com>
2025-08-30 15:56:51 +0500

    renamed filter.go to message_filter.go

--- filter/message_filter.go
+++ filter/message_filter.go
@@ -8,28 +8,28 @@ import (
 	"github.com/purkhanov/gogram/types"
 )
 
-type Filter func(*types.Message) bool
+type MessageFilter func(*types.Message) bool
 
-func TextEquals(text string) Filter {
+func TextEquals(text string) MessageFilter {
 	return func(m *types.Message) bool {
 		return m.Text == text
 	}
 }
 
-func TextContains(substring string) Filter {
+func TextContains(substring string) MessageFilter {
 	return func(m *types.Message) bool {
 		return strings.Contains(m.Text, substring)
 	}
 }
 
-func TextMatches(pattern string) Filter {
+func TextMatches(pattern string) MessageFilter {
 	return func(m *types.Message) bool {
 		matched, _ := regexp.MatchString(pattern, m.Text)
 		return matched
 	}
 }
 
-func IsCommand(command commands.Command) Filter {
+func IsCommand(command commands.Command) MessageFilter {
 	return func(m *types.Message) bool {
 		return m.Text == string(command)
 	}
