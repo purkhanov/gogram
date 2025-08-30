632f209d537d3f532fc43628da1ad80a4e15225a
Nurlan Purkhanov <ppurkhanov@gmail.com>
2025-08-30 15:55:34 +0500

    created message handler and removed handler

---
+++ dispatcher/message_handler.go
@@ -0,0 +1,51 @@
+package dispatcher
+
+import (
+	"log"
+
+	"github.com/purkhanov/gogram/commands"
+	filters "github.com/purkhanov/gogram/filter"
+	"github.com/purkhanov/gogram/types"
+)
+
+type messageHandlerFunc func(*types.Message) error
+
+type messageHandler struct {
+	filters []filters.MessageFilter
+	handler messageHandlerFunc
+}
+
+func (d *dispatcher) CommandHandler(msgHandler messageHandlerFunc, command commands.Command) {
+	d.handlers.messageHandler = append(d.handlers.messageHandler, messageHandler{
+		filters: []filters.MessageFilter{filters.IsCommand(command)},
+		handler: msgHandler,
+	})
+}
+
+func (d *dispatcher) MessageHandler(msgHandler messageHandlerFunc, filters ...filters.MessageFilter) {
+	d.handlers.messageHandler = append(d.handlers.messageHandler, messageHandler{
+		filters: filters,
+		handler: msgHandler,
+	})
+}
+
+func (d *dispatcher) messageHandler(msg *types.Message) {
+	for _, msgHandler := range d.handlers.messageHandler {
+		matches := true
+
+		for _, filter := range msgHandler.filters {
+			if !filter(msg) {
+				matches = false
+				break
+			}
+		}
+
+		if !matches {
+			continue
+		}
+
+		if err := msgHandler.handler(msg); err != nil {
+			log.Printf("error handling update: %v", err)
+		}
+	}
+}
