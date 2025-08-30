5bd9b93c43cfb31e4cbf790c51fa2fece7cd92cd
Nurlan Purkhanov <ppurkhanov@gmail.com>
2025-08-30 19:48:57 +0500

    created context to bot

--- bot/bot.go
+++ bot/bot.go
@@ -1,27 +1,34 @@
 package bot
 
 import (
+	"context"
 	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/purkhanov/gogram/api"
 )
 
 const (
-	baseUrl         = "https://api.telegram.org"
-	contentTypeJSON = "application/json"
+	baseUrl            = "https://api.telegram.org"
+	contentTypeJSON    = "application/json"
+	httpRequestTimeout = 5 * time.Second
 )
 
 type Bot struct {
 	token        string
 	urlWithToken string
 	api          *api.ApiClient
+	ctx          context.Context
 }
 
 func NewBot(token string) *Bot {
+	ctx := context.Background()
+
 	return &Bot{
 		token:        token,
 		urlWithToken: fmt.Sprintf("%s/bot%s", baseUrl, token),
 		api:          api.NewClient(&http.Client{}),
+		ctx:          ctx,
 	}
 }
