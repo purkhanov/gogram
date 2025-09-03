405539d70a790b897a79fc3728733e8b5dd9d18f
Nurlan Purkhanov <ppurkhanov@gmail.com>
2025-09-03 23:21:27 +0500

    created CreateInvoiceLink and AnswerShippingQuery logic

--- bot/payments.go
+++ bot/payments.go
@@ -13,6 +13,8 @@ import (
 const (
 	sendInvoiceUrl            = "/sendInvoice"
 	answerPreCheckoutQueryUrl = "/answerPreCheckoutQuery"
+	createInvoiceLinkUrl      = "/createInvoiceLink"
+	answerShippingQueryUrl    = "/answerShippingQuery"
 )
 
 type SendInvoiceOptions struct {
@@ -235,5 +237,202 @@ func (b *Bot) AnswerPreCheckoutQuery(params AnswerPreCheckoutQueryParams) error 
 		return fmt.Errorf("telegram API error: code %d - %s", result.ErrorCode, result.Description)
 	}
 
+	return nil
+}
+
+type CreateInvoiceLinkOptions struct {
+	// Unique identifier of the business connection on behalf of which
+	// the link will be created. For payments in Telegram Stars only.
+	BusinessConnectionID string `json:"business_connection_id,omitempty"`
+
+	// Product name, 1-32 characters
+	Title string `json:"title" validate:"required"`
+
+	// Product description, 1-255 characters
+	Description string `json:"description" validate:"required"`
+
+	// Bot-defined invoice payload, 1-128 bytes. This will not be
+	// displayed to the user, use it for your internal processes.
+	Payload string `json:"payload" validate:"required"`
+
+	// Payment provider token, obtained via @BotFather.
+	// Pass an empty string for payments in Telegram Stars.
+	ProviderToken string `json:"provider_token,omitempty"`
+
+	// Three-letter ISO 4217 currency code, see more on currencies
+	// (https://core.telegram.org/bots/payments#supported-currencies)
+	// Pass “XTR” for payments in Telegram Stars.
+	Currency string `json:"currency" validate:"required"`
+
+	// rice breakdown, a JSON-serialized list of components
+	//  (e.g. product price, tax, discount, delivery cost,
+	// delivery tax, bonus, etc.). Must contain exactly one
+	// item for payments in Telegram Stars.
+	Prices []types.LabeledPrice `json:"prices" validate:"required"`
+
+	// The number of seconds the subscription will be active for
+	// before the next payment. The currency must be set to “XTR”
+	// (Telegram Stars) if the parameter is used. Currently, it must
+	// always be 2592000 (30 days) if specified. Any number of
+	// subscriptions can be active for a given bot at the same time,
+	// including multiple concurrent subscriptions from the same user.
+	// Subscription price must no exceed 10000 Telegram Stars.
+	SubscriptionPeriod uint32 `json:"subscription_period,omitempty"`
+
+	// The maximum accepted amount for tips in the smallest units
+	// of the currency (integer, not float/double). For example,
+	// for a maximum tip of US$ 1.45 pass max_tip_amount = 145.
+	// See the exp parameter in currencies.json, it shows the
+	// number of digits past the decimal point for each currency
+	// (2 for the majority of currencies). Defaults to 0.
+	// Not supported for payments in Telegram Stars.
+	MaxTipAmount uint32 `json:"max_tip_amount,omitempty"`
+
+	// A JSON-serialized array of suggested amounts of tips in the smallest
+	// units of the currency (integer, not float/double). At most 4 suggested
+	// tip amounts can be specified. The suggested tip amounts must be positive,
+	// passed in a strictly increased order and must not exceed max_tip_amount.
+	SuggestedTipAmounts []uint32 `json:"suggested_tip_amounts,omitempty"`
+
+	// JSON-serialized data about the invoice, which will be shared
+	// with the payment provider. A detailed description of required
+	// fields should be provided by the payment provider.
+	ProviderData string `json:"provider_data,omitempty"`
+
+	// URL of the product photo for the invoice. Can be a
+	// photo of the goods or a marketing image for a service.
+	PhotoUrl string `json:"photo_url,omitempty"`
+
+	// Photo size in bytes
+	PhotoSize uint16 `json:"photo_size,omitempty"`
+
+	//  	Photo width
+	Photo_width uint16 `json:"photo_width,omitempty"`
+
+	// Photo height
+	PhotoHeight uint16 `json:"photo_height,omitempty"`
+
+	// Pass True if you require the user's full name to complete
+	// the order. Ignored for payments in Telegram Stars.
+	NeedName bool `json:"need_name,omitempty"`
+
+	// Pass True if you require the user's phone number to complete
+	// the order. Ignored for payments in Telegram Stars.
+	NeedPhoneNumber bool `json:"need_phone_number,omitempty"`
+
+	// Pass True if you require the user's email address to complete
+	// the order. Ignored for payments in Telegram Stars.
+	NeedEmail bool `json:"need_email,omitempty"`
+
+	// Pass True if you require the user's shipping address to
+	// complete the order. Ignored for payments in Telegram Stars.
+	NeedShippingAddress bool `json:"need_shipping_address,omitempty"`
+
+	// Pass True if the user's phone number should be sent to
+	// the provider. Ignored for payments in Telegram Stars.
+	SendPhoneNumberToProvider bool `json:"send_phone_number_to_provider,omitempty"`
+
+	// Pass True if the user's email address should be sent to
+	// the provider. Ignored for payments in Telegram Stars.
+	SendEmailToProvider bool `json:"send_email_to_provider,omitempty"`
+
+	// Pass True if the final price depends on the shipping
+	// method. Ignored for payments in Telegram Stars.
+	IsFlexible bool `json:"is_flexible,omitempty"`
+}
+
+func (b *Bot) CreateInvoiceLink(params CreateInvoiceLinkOptions) (string, error) {
+	if err := utils.ValidateStruct(params); err != nil {
+		return "", err
+	}
+
+	data, err := json.Marshal(params)
+	if err != nil {
+		return "", err
+	}
+
+	c, cancel := context.WithTimeout(b.Ctx, httpRequestTimeout)
+	defer cancel()
+
+	resp, err := b.api.DoRequestWithContextAndData(
+		c, http.MethodPost, b.urlWithToken+createInvoiceLinkUrl, data,
+	)
+	if err != nil {
+		return "", err
+	}
+
+	var result types.APIResponse[string]
+
+	if err := json.Unmarshal(resp, &result); err != nil {
+		return "", err
+	}
+
+	if !result.Ok {
+		return "", fmt.Errorf(
+			"telegram API error: code %d - %s", result.ErrorCode, result.Description,
+		)
+	}
+
+	return result.Result, nil
+}
+
+type AnswerShippingQueryOptions struct {
+	// Unique identifier for the query to be answered
+	ShippingQueryID string `json:"shipping_query_id" validate:"required"`
+
+	// Pass True if delivery to the specified address is possible
+	// and False if there are any problems (for example, if
+	// delivery to the specified address is not possible)
+	Ok bool `json:"ok" validate:"required"`
+
+	// Required if ok is True. A JSON-serialized array of available shipping options.
+	ShippingOptions []types.ShippingOption `json:"shipping_options,omitempty"`
+
+	// Required if ok is False. Error message in human readable
+	// form that explains why it is impossible to complete the
+	// order (e.g. “Sorry, delivery to your desired address is
+	// unavailable”). Telegram will display this message to the user.
+	ErrorMessage string `json:"error_message,omitempty"`
+}
+
+func (b *Bot) AnswerShippingQuery(params AnswerShippingQueryOptions) error {
+	if len(params.ShippingQueryID) == 0 {
+		return fmt.Errorf("shipping_query_id is required")
+	}
+
+	if params.Ok && len(params.ShippingOptions) == 0 {
+		return fmt.Errorf("shipping_options are required when ok is true")
+	}
+
+	if !params.Ok && len(params.ErrorMessage) == 0 {
+		return fmt.Errorf("error_message is required when ok is false")
+	}
+
+	data, err := json.Marshal(params)
+	if err != nil {
+		return err
+	}
+
+	c, cancel := context.WithTimeout(b.Ctx, httpRequestTimeout)
+	defer cancel()
+
+	resp, err := b.api.DoRequestWithContextAndData(
+		c, http.MethodPost, b.urlWithToken+answerShippingQueryUrl, data,
+	)
+	if err != nil {
+		return err
+	}
+
+	var result types.APIResponse[bool]
+
+	if err := json.Unmarshal(resp, &result); err != nil {
+		return err
+	}
+
+	if !result.Ok {
+		return fmt.Errorf(
+			"telegram API error: code %d - %s", result.ErrorCode, result.Description,
+		)
+	}
 	return nil
 }
