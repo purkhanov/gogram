5bf4f93f057a05315fe6bc3c10d5246ae1b19f57
Nurlan Purkhanov <ppurkhanov@gmail.com>
2025-09-10 23:14:05 +0500

    created ChatShared type

--- types/chat.go
+++ types/chat.go
@@ -103,6 +103,57 @@ type ChatInviteLink struct {
 	SubscriptionPrice int `json:"subscription_price,omitempty"`
 }
 
+// This object contains information about a chat that was
+// shared with the bot using a KeyboardButtonRequestChat button.
+type ChatShared struct {
+	// Identifier of the request
+	RequestID int `json:"request_id"`
+
+	// Identifier of the shared chat. This number may
+	// have more than 32 significant bits and some
+	// programming languages may have difficulty/silent
+	// defects in interpreting it. But it has at most 52
+	// significant bits, so a 64-bit integer or double-precision
+	// float type are safe for storing this identifier.
+	// The bot may not have access to the chat and could
+	// be unable to use this identifier, unless the chat
+	// is already known to the bot by some other means.
+	ChatID int `json:"chat_id"`
+
+	// Optional. Title of the chat, if the title was requested by the bot.
+	Title string `json:"title,omitempty"`
+
+	// Optional. Username of the chat, if the
+	// username was requested by the bot and available.
+	Username string `json:"username,omitempty"`
+
+	// Optional. Available sizes of the chat photo,
+	// if the photo was requested by the bot
+	Photo []PhotoSize `json:"photo,omitempty"`
+}
+
+// This object represents a service message about a
+// user allowing a bot to write messages after adding
+// it to the attachment menu, launching a Web App
+// from a link, or accepting an explicit request from
+// a Web App sent by the method requestWriteAccess.
+// (https://core.telegram.org/bots/webapps#initializing-mini-apps)
+type WriteAccessAllowed struct {
+	// Optional. True, if the access was granted
+	// after the user accepted an explicit request
+	// from a Web App sent by the method requestWriteAccess
+	// (https://core.telegram.org/bots/webapps#initializing-mini-apps)
+	FromRequest bool `json:"from_request,omitempty"`
+
+	// Optional. Name of the Web App, if the access was
+	// granted when the Web App was launched from a link
+	WebAppName string `json:"web_app_name,omitempty"`
+
+	// Optional. True, if the access was granted when
+	// the bot was added to the attachment or side menu
+	FromAttachmentMenu bool `json:"from_attachment_menu,omitempty"`
+}
+
 type ChatJoinRequest struct {
 	// Chat to which the request was sent
 	Chat Chat `json:"chat"`
