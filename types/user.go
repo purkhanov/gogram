70d9bdd161dc92a1bbb06e0d6c60b2ba8c967485
Nurlan Purkhanov <ppurkhanov@gmail.com>
2025-09-10 15:41:54 +0500

    location moved to user file and created Contact type

--- types/user.go
+++ types/user.go
@@ -46,3 +46,80 @@ type User struct {
 	// Optional. True, if the bot has a main Web App. Returned only in getMe.
 	HasMainWebApp bool `json:"has_main_web_app,omitempty"`
 }
+
+// This object represents a phone contact.
+type Contact struct {
+	// Contact's phone number
+	PhoneNumber string `json:"phone_number"`
+
+	// Contact's first name
+	FirstName string `json:"first_name"`
+
+	// Optional. Contact's last name
+	LastName string `json:"last_name,omitempty"`
+
+	// Optional. Contact's user identifier in Telegram.
+	// This number may have more than 32 significant bits
+	// and some programming languages may have difficulty/silent
+	// defects in interpreting it. But it has at most 52
+	// significant bits, so a 64-bit integer or double-precision
+	// float type are safe for storing this identifier.
+	UserID int `json:"user_id,omitempty"`
+
+	// Optional. Additional data about the contact in the form of a vCard
+	// (https://en.wikipedia.org/wiki/VCard)
+	Vcard string `json:"vcard,omitempty"`
+}
+
+type Location struct {
+	// Latitude as defined by the sender
+	Latitude float64 `json:"latitude"`
+
+	// Longitude as defined by the sender
+	Longitude float64 `json:"longitude"`
+
+	// Optional. The radius of uncertainty for the location,
+	// measured in meters; 0-1500
+	HorizontalAccuracy float64 `json:"horizontal_accuracy,omitempty"`
+
+	// Optional. Time relative to the message sending date,
+	// during which the location can be updated; in seconds.
+	// For active live locations only.
+	LivePeriod int `json:"live_period,omitempty"`
+
+	// Optional. The direction in which user is moving,
+	// in degrees; 1-360. For active live locations only.
+	Heading int `json:"heading,omitempty"`
+
+	// Optional. The maximum distance for proximity alerts
+	// about approaching another chat member, in meters.
+	// For sent live locations only.
+	ProximityAlertRadius int `json:"proximity_alert_radius,omitempty"`
+}
+
+// This object represents a venue.
+type Venue struct {
+	// Venue location. Can't be a live location
+	Location *Location `json:"location"`
+
+	// Name of the venue
+	Title string `json:"title"`
+
+	// Address of the venue
+	Address string `json:"address"`
+
+	// Optional. Foursquare identifier of the venue
+	FoursquareID string `json:"foursquare_id,omitempty"`
+
+	// Optional. Foursquare type of the venue. (For example,
+	// “arts_entertainment/default”, “arts_entertainment/aquarium”
+	// or “food/icecream”.)
+	FoursquareType string `json:"foursquare_type,omitempty"`
+
+	// Optional. Google Places identifier of the venue
+	GooglePlaceID string `json:"google_place_id,omitempty"`
+
+	// Optional. Google Places type of the venue. (See supported types.)
+	// (https://developers.google.com/maps/documentation/places/web-service/legacy/supported_types)
+	GooglePlaceType string `json:"google_place_type,omitempty"`
+}
